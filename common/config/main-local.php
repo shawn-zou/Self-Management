2534804a4db64921af942b00b3606a01cb142f65
shawn zou <157962718@qq.com>
2016-05-18 17:28:23 +0800

    先尝试配置I18n

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,9 +3,14 @@
  * @author shawn-zou <1579627187@qq.com> 2016年5月18日
  */
 return [
-    'language' => 'zh-CN',    //语言
-    //'defaultRoute' => '/user/login',    //前后台都是登录页面所以在公共设置里设置
-    //'homeUrl' => '',    //首页Home设置
+    //设置目标语言
+    'language' => 'zh-CN',
+    //设置源语言，文档建议用默认的en-US,说是别的语言翻译起来方便。。。干
+    //'sourceLanguage' => 'zh-CN',
+    //前后台的默认首页路由是一样的，所以在公共配置里设置
+    //'defaultRoute' => '/user/login',
+    //设置跳转的页面，$this->goHome()方法要的配置
+    //'homeUrl' => '',
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
@@ -22,6 +27,20 @@ return [
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
             'useFileTransport' => true,
+        ],
+        //国际化I18N配置
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@common/language',
+                    //'sourceLanguage' => 'en-US',
+                    'fileMap' => [
+                        'app' => 'app.php',
+                        'app/error' => 'error.php',
+                    ],
+                ],
+            ],
         ],
     ],
 ];
